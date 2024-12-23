fc51a79b41bf15ac79dbb58ffd127c03cfbec048
Pratik Kumar <134793499+PratikKumarJha@users.noreply.github.com>
2024-12-23 10:31:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-const avatarSrc = "DEVIL.png";
+const avatarSrc = "DEVIL.jpg";
 
 const Footer = () => {
   return (
